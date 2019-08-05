f5e8d1b7fae3a0a58daacd45c5d3667ca0cca26a
Shuja Ud Din <shuja.uddin@virtualforce.io>
2019-08-05 09:41:48 +0500

    Initial Commit

---
+++ AzureTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AzureTest
+//
+//  Created by Shuja ud Din on 05/08/2019.
+//  Copyright © 2019 Shuja ud Din. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
